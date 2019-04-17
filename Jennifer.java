1b7eeab9b943f99761fe3481165620897631ec62
PaulWoode <woode.paul@gmail.com>
2019-04-16 23:47:08 -0400

    Delete Jennifer.java

--- Jennifer.java
+++
@@ -1,8 +0,0 @@
-// My first program 
-public class HelloWorld {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
-}
-
